8f02fcc28159c6c18ceda05c26c06cb4a1c72a85
马剑峰 <18303011729@163.com>
2017-10-27 18:07:43 +0800

    Update changeFlie.js

--- changeFlie.js
+++ changeFlie.js
@@ -19,13 +19,3 @@ fs.readdir(path, function(err, files) {
         })
     })
 })  
-function Trim(str,is_global)
-        {
-            var result;
-            result = str.replace(/(^\s+)|(\s+$)/g,"");
-            if(is_global.toLowerCase()=="g")
-            {
-                result = result.replace(/\s/g,"");
-             }
-            return result;
-}  
