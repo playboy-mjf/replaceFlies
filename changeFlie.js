3475681d2de363fbbe98535a0fc8477b6875954e
unknown <mjf@DESKTOP-2LK0O6M.(none)>
2017-10-27 17:51:14 +0800

    first commit

---
+++ changeFlie.js
@@ -0,0 +1,31 @@
+// 引入fs文件处理模块
+var fs = require("fs");
+var path = 'img'
+fs.readdir(path, function(err, files) {
+
+    // files是名称数组
+    files.forEach(function(filename) {
+
+        //console.log(filename)
+        //运用正则表达式替换oldPath中不想要的部分
+        var oldPath = path + '/' + filename,
+        newPath = path + '/' + filename.replace(/\s/g,'')
+
+        // fs.rename(oldPath, newPath, callback) 
+        fs.rename(oldPath, newPath, function(err) {
+            if (!err) {
+                console.log(filename + 'ok!')
+            } 
+        })
+    })
+})  
+function Trim(str,is_global)
+        {
+            var result;
+            result = str.replace(/(^\s+)|(\s+$)/g,"");
+            if(is_global.toLowerCase()=="g")
+            {
+                result = result.replace(/\s/g,"");
+             }
+            return result;
+}  
